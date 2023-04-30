e9473e7c7712d4e2e015f1b07ddb46b3f6320e37
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-04-30 23:08:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
+import "./styles/App.scss";
+import "./styles/Header.scss";
+import "./styles/Home.scss";
+import "./styles/Footer.scss";
+import "./styles/Contact.scss";
+import "./styles/Media.scss";
 
+import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/services" element={<Services></Services>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </Router>
   );
 }
 
